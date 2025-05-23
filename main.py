a96533e75eda011a8f5bbc420e68bec110a6fec1
Evoleaves <enlacruzdepiedra@gmail.com>
2025-05-23 17:57:53 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routers import varieties
+import os
+import uvicorn
+
+app = FastAPI(title="Cannacore API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(varieties.router)
+
+@app.get("/")
+def root():
+    return {"message": "Cannacore API demo funcionando correctamente"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
